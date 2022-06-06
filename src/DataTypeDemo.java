bbcd76b08f0c7a1fa24b46c235c396fa97225305
amolu <amolujagare@gmail.com>
2022-06-06 22:02:25 +0530

    Initial commit

---
+++ src/DataTypeDemo.java
@@ -0,0 +1,24 @@
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+
+       // byte b = -129;
+
+       // short s = -32769 ;
+
+        float f = 15.8f;
+
+        double d = 12.6;
+        double d1 = 15.8;
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        char c = 'h';
+
+        int x = 'h';
+        char c1 = 45;
+
+
+    }
+}
---
+++ src/TesstCharacter.java
@@ -0,0 +1,14 @@
+public class TesstCharacter {
+
+    public static void main(String[] args) {
+
+
+       // char c = 65;
+
+      //  System.out.println("c="+c);
+
+        int a = 'f';
+        System.out.println("a="+a);
+
+    }
+}
